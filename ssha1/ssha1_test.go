6630cd886d954da8c091dbd38ee9eba3c690fbc0
Kristin Jeanna <67802120+kristinjeanna@users.noreply.github.com>
2022-03-28 09:25:26 -0500

    More tests for coverage.

--- ssha1/ssha1_test.go
+++ ssha1/ssha1_test.go
@@ -17,25 +17,37 @@ func TestSum(t *testing.T) {
 	sumCases := []sumCase{
 		{[]byte("supercalifragilisticexpialidocious"), []byte("n4pggXWL"), "8eadde532169b6908034886be119c9f0ca61801e6e3470676758574c"},
 		{[]byte("abcdefghijklmnopqrstuvwxyz"), []byte("K218iReB"), "4ced2536edce6706cccf0c14a10a939022f6b0614b32313869526542"},
+		{[]byte("All things are strange which are worth knowing."), nil, ""}, // coverage
+		{[]byte("Who you are authentically is alright."), []byte{}, ""},      // coverage
 	}
 
 	for _, c := range sumCases {
-		result, err := Sum(c.plaintext, c.salt)
-		if err != nil {
-			t.Errorf("method Sum() returned unexpected error: %e", err)
-		}
-		resultString := hex.EncodeToString(result)
-		if resultString != c.expectedHexString {
-			t.Errorf("result = %s; expected %s", resultString, c.expectedHexString)
+		if c.salt == nil { // for coverage
+			Sum(c.plaintext, c.salt)
+		} else if len(c.salt) == 0 { // should produce err due to 0-length salt
+			_, err := Sum(c.plaintext, c.salt)
+			if err == nil {
+				t.Errorf("method Sum() failed to return expected error")
+			}
+		} else {
+			result, err := Sum(c.plaintext, c.salt)
+			if err != nil {
+				t.Errorf("method Sum() returned unexpected error: %e", err)
+			}
+			resultString := hex.EncodeToString(result)
+			if resultString != c.expectedHexString {
+				t.Errorf("result = %s; expected %s", resultString, c.expectedHexString)
+			}
 		}
 	}
 }
 
 type sizeCase struct {
-	newMethod  string
-	h          hash.Hash
-	errFromNew error
-	expected   int
+	newMethod   string
+	h           hash.Hash
+	errFromNew  error
+	expected    int
+	expectError bool
 }
 
 func setUpSizeCases() []sizeCase {
@@ -43,20 +55,29 @@ func setUpSizeCases() []sizeCase {
 	c1.newMethod = "New()"
 	c1.h, c1.errFromNew = New()
 	c1.expected = sha1.Size + DefaultNumSaltBytes
+	c1.expectError = false
 
 	var c2 sizeCase
 	c2.newMethod = "NewForSaltSize()"
 	c2.h, c2.errFromNew = NewForSaltSize(32)
 	c2.expected = sha1.Size + 32
+	c2.expectError = false
 
 	var c3 sizeCase
+	c3.newMethod = "NewForSaltSize()"
+	c3.h, c3.errFromNew = NewForSaltSize(0) // invalid salt size
+	c3.expected = 0
+	c3.expectError = true
+
+	var c4 sizeCase
 	salt1 := []byte("2cM6D2WitazRL5MD")
-	c3.newMethod = "NewWithSalt()"
-	c3.h, c3.errFromNew = NewWithSalt(salt1)
-	c3.expected = sha1.Size + len(salt1)
+	c4.newMethod = "NewWithSalt()"
+	c4.h, c4.errFromNew = NewWithSalt(salt1)
+	c4.expected = sha1.Size + len(salt1)
+	c4.expectError = false
 
 	cases := make([]sizeCase, 0)
-	cases = append(cases, c1, c2, c3)
+	cases = append(cases, c1, c2, c3, c4)
 
 	return cases
 }
@@ -65,11 +86,14 @@ func TestSize(t *testing.T) {
 	cases := setUpSizeCases()
 
 	for _, c := range cases {
-		if c.errFromNew != nil {
+		if c.expectError {
+			if c.errFromNew == nil {
+				t.Errorf("expected error but none returned for test case: %v", c)
+			}
+		} else if c.errFromNew != nil {
 			t.Errorf("%s returned unexpected error: %e", c.newMethod, c.errFromNew)
-		}
-		if got := c.h.Size(); got != c.expected {
-			t.Errorf("for test case %v: Size = %d; expected %d", c, got, c.expected)
+		} else if result := c.h.Size(); result != c.expected {
+			t.Errorf("for test case %v: Size = %d; expected %d", c, result, c.expected)
 		}
 	}
 }
@@ -122,7 +146,37 @@ func TestBlockSize(t *testing.T) {
 	if err != nil {
 		t.Errorf("method New() returned unexpected error: %e", err)
 	}
-	if got := c.BlockSize(); got != BlockSize {
-		t.Errorf("BlockSize = %d; expected %d", got, BlockSize)
+	if result := c.BlockSize(); result != BlockSize {
+		t.Errorf("BlockSize result = %d; expected %d", result, BlockSize)
+	}
+}
+
+func TestHexString(t *testing.T) {
+	c, err := NewWithSalt([]byte("ajE94aZM"))
+	if err != nil {
+		t.Errorf("method New() returned unexpected error: %e", err)
+	}
+
+	expected := "294ac58b8b662e8f604fcf6ea4ca01105d580083616a453934615a4d"
+
+	c.Write([]byte("When life gives you lemons, make lemonade."))
+
+	if result := c.HexString(); result != expected {
+		t.Errorf("HexString result = %s; expected %s", result, expected)
+	}
+}
+
+func TestString(t *testing.T) {
+	c, err := NewWithSalt([]byte("R*w.5Vmo"))
+	if err != nil {
+		t.Errorf("method New() returned unexpected error: %e", err)
+	}
+
+	expected := "{SSHA}h+WWKpgLY/OQorn+uHAi7Gsr9LZSKncuNVZtbw=="
+
+	c.Write([]byte("You have to be odd to be number one."))
+
+	if result := c.String(); result != expected {
+		t.Errorf("String result = %s; expected %s", result, expected)
 	}
 }
